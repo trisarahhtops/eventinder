fb640ece7d44960d49487b2d3d96d2d587a8323f
Amelie Patzer <140917470+uxspark@users.noreply.github.com>
2024-04-22 10:54:54 +1000

    created the xcode project

---
+++ EventTinder/EventTinder/EventTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventTinderApp.swift
+//  EventTinder
+//
+//  Created by Amelie Patzer on 22.04.24.
+//
+
+import SwiftUI
+
+@main
+struct EventTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
