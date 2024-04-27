c73355a5a649872d478d76c0b3b221ae4e3dff78
trisarahhtops <sarah.zhong19@gmail.com>
2024-04-27 16:29:23 +1000

    moved matchesview to view folder

---
+++ EventTinder/EventTinder/View/DecisionsView.swift
@@ -0,0 +1,18 @@
+//
+//  DecisionsView.swift
+//  EventTinder
+//
+//  Created by Sarah Zhong on 27/4/2024.
+//
+
+import SwiftUI
+
+struct DecisionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DecisionsView()
+}
