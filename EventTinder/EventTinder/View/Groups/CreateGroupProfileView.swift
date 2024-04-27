d34c12b9c4b207769647151d618d47bf8e02bc14
Amelie Patzer <140917470+uxspark@users.noreply.github.com>
2024-04-27 18:03:19 +1000

    added a create group view for selecting the group picture

--- EventTinder/EventTinder/View/Groups/CreateGroupProfileView.swift
+++ EventTinder/EventTinder/View/Groups/CreateGroupProfileView.swift
@@ -8,8 +8,68 @@
 import SwiftUI
 
 struct CreateGroupProfileView: View {
+    @State private var groupPicture: Image = Image(.group1)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Text("New Group")
+                .font(.title)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Text("Choose a group picture")
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Spacer()
+            groupPicture
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width-40, height: UIScreen.main.bounds.width-40)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            Spacer()
+            HStack {
+                Button {
+                    groupPicture = Image(.group1)
+                } label: {
+                    Image(.group1)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width/4-16, height: UIScreen.main.bounds.width/4-16)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+                Button {
+                    groupPicture = Image(.group2)
+                } label: {
+                    Image(.group2)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width/4-16, height: UIScreen.main.bounds.width/4-16)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+                Button {
+                    groupPicture = Image(.group3)
+                } label: {
+                    Image(.group3)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width/4-16, height: UIScreen.main.bounds.width/4-16)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+                Button {
+                    groupPicture = Image(.group4)
+                } label: {
+                    Image(.group4)
+                        .resizable()
+                        .frame(width: UIScreen.main.bounds.width/4-16, height: UIScreen.main.bounds.width/4-16)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+            }
+            Spacer()
+            NavigationLink("Create Group") {
+                
+            }
+            .font(.headline)
+            .foregroundColor(.white)
+            .frame(height: 55)
+            .frame(maxWidth: .infinity)
+            .background(Color.accentColor)
+            .cornerRadius(10)
+        }
+        .padding(20)
     }
 }
 
