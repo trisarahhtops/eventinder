72be642734a0ab5981a465c76b6b2c8d72643ed4
trisarahhtops <sarah.zhong19@gmail.com>
2024-04-27 16:27:09 +1000

    MatchesView

---
+++ EventTinder/EventTinder/MatchesView.swift
@@ -0,0 +1,18 @@
+//
+//  MatchesView.swift
+//  EventTinder
+//
+//  Created by Sarah Zhong on 27/4/2024.
+//
+
+import SwiftUI
+
+struct MatchesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchesView()
+}
