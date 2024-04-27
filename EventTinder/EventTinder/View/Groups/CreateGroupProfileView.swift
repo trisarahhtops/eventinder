e71fcce23165871d539c2d2de758793d02df1b7d
Amelie Patzer <140917470+uxspark@users.noreply.github.com>
2024-04-27 15:44:41 +1000

    build UI for Create Group View of selecting the friends, searchbar isn't showing yet
    TODO fix: searchbar not showing when accessed through contentview

---
+++ EventTinder/EventTinder/View/Groups/CreateGroupProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateGroupProfileView.swift
+//  EventTinder
+//
+//  Created by Amelie Patzer on 27.04.24.
+//
+
+import SwiftUI
+
+struct CreateGroupProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateGroupProfileView()
+}
