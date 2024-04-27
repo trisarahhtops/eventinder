e60a47f48618b20e7f7e997c920df6034e3df8aa
trisarahhtops <sarah.zhong19@gmail.com>
2024-04-27 16:40:18 +1000

    eventinderapp

---
+++ EventTinder/EventTinder/View/GroupView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupView.swift
+//  EventTinder
+//
+//  Created by Sarah Zhong on 27/4/2024.
+//
+
+import SwiftUI
+
+struct GroupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupView()
+}
