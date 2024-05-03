a35c856eb6f74ec8084af09e13d015bd407d482c
juliaseisele <167724299+juliaseisele@users.noreply.github.com>
2024-05-03 12:45:04 +1000

    Cleaned code up

--- EventTinder/EventTinder/ViewModel/UserManagerViewModel.swift
+++ EventTinder/EventTinder/ViewModel/UserManagerViewModel.swift
@@ -9,8 +9,8 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-struct User {
-    let userId: String
+struct User: Codable {
+    let uid: String
     let email: String?
     let photoURL: String?
 }
@@ -19,31 +19,30 @@ struct User {
 final class UserManagerViewModel {
     static let shared = UserManagerViewModel()
     
-    func createNewUser(auth: AuthDataResultModel) async throws{
-    //use to add a user to the database: UserManager.shared.createnewUser(auth: authDataResult)
-        var userData: [String:Any] = ["user_Id" : auth.uid, "email": auth.email ?? "", "photoURL": auth.photoUrl ?? ""]
-        try await Firestore.firestore().collection("users").document(auth.uid).setData(userData, merge: false)
+    private let userCollection = Firestore.firestore().collection("users")
+    
+    private func userDocument(userId: String) -> DocumentReference {
+        userCollection.document(userId)
+    }
+    
+    func createNewUser(user: User) async throws{
+    //use to add a user to the database:
+        //let user = User(userId: someString, email: optionalString, photoURL: optionalString)
+        //UserManager.shared.createnewUser(user: user)
+
+        try userDocument(userId: user.uid).setData(from: user, merge: false)
     }
     
     func getUser(userId: String) async throws -> User {
     //uses an userID to get the data from the user. returns type struct User
     //use: try await UserManager.shared.getUser(userId: someString)
         
-        let snapshot = try await Firestore.firestore().collection("users").document(userId).getDocument()
-        guard let data = snapshot.data(), let userId = data["user_id"] else {
-            throw URLError(.badServerResponse)
-        }
-        let email = data["email"] as? String
-        let photoURL = data["photoURL"] as? String
-        
-        return User(userId: userId as! String, email: email, photoURL: photoURL)
+        let snapshot = try await userDocument(userId: userId).getDocument(as: User.self)
+        return snapshot
     }
     
-    func searchUser(searchStr: String){
-        Firestore.firestore().collection("users")
-            .order(by: "user_id")
-            .start(at: [searchStr])
-            .limit(to: 10)
+    func searchUser(searchStr: String) async throws {
+        try await Firestore.firestore().collection("users").whereField("user_id", arrayContains: searchStr).getDocuments()
     }
     
 }
